453be6fcf932df11389c2d3781f1f2ec0266fbe5
Канами Тобэ <unpredictable573@gmail.com>
2017-10-21 20:37:52 +0300

    tokeniser.py

--- tokeniser.py
+++ tokeniser.py
@@ -1,16 +1,22 @@
 import sys
 text = sys.stdin.readlines()
 e=1
+
 for line in text:
-	print()
 	line = line.replace("."," .").replace(".", " .") 
 	token = line.strip().split(' ')
+	line = line.strip(' ')
+	if line == '':
+		continue
 	orders = 1 
 	print("# sent_id =",e)
 	print("# text =", line.strip()) 
 	for t in token:
+		if t.strip() == '':
+			continue
+	
 		print("%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_" % (orders, t))
 		orders = orders + 1
 	e=e+1
-	print()
-	
+
+print()
