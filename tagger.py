b2c4a4b22aac445a8d36f93f078d388129f66356
Канами Тобэ <unpredictable573@gmail.com>
2017-10-24 21:13:33 +0300

    tagger.py

--- tagger.py
+++ tagger.py
@@ -1,24 +1,53 @@
 import sys
 
 fd = open('train.output')
+
+frequent_tab = "X" #variable to store the most frequent tab we have seen
+last_max = 0
+
+
+word_tag = {}
+word_prob = {}
+
 for line in  fd.readlines():
 	line = line.strip('\n')
 	line = line.split('\t')
-	if int(row[1]) > count:
-		print(line)
-		count= int(line[1])
-	print(line)
-	
-input = sys.stdin.readlines()
 
+
+	if int(line[1]) > last_max:
+		
+		last_max = int(line[1])
+		frequent_tag = line[2]
+	
+	surface_form = line[3]
+	prob = float(line[0])
+	tag = line[2]	
+		
+	if surface_form not in word_tag:
+		word_tag[surface_form] = tag
+		word_prob[surface_form] = prob
+		
+		#if we have seen the surface form before
+	if surface_form in word_tag:
+		if prob > word_prob[surface_form]:
+			word_tag[surface_form] = tag
+			word_prob[surface_form] = prob
+		
+input = sys.stdin.readlines()
+ 
 for line in input:
+	line = line.strip()#strip off spaces
 	if '\t' not in line:
+		print(line)
 		continue
-	row = line.split('\t')
-	x='X'
-	row[3]=x
+	line = line.split('\t')
 	
+	if line[1] in word_tag:
+		line[3] = word_prob[line[1]]
+	else:
+		line[3] = frequent_tag
+		
+	line[3] = frequent_tag
 	
-	print(row)
-	
+	print('\t'.join(line))	
 	
