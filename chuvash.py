a26ccde1b3d34dc8c02fa24d97a199c6776ff8e4
Канами Тобэ <unpredictable573@gmail.com>
2017-10-27 19:48:06 +0300

    chuvash.py

--- chuvash.py
+++ chuvash.py
@@ -7,17 +7,27 @@ cases = ['ăн','ĕн','нăн','нĕн','а','е','на','не','ра','ре','
 #  1л. -(ă)п/-(ĕ)п/-(ă)м/-(ĕ)м  2л. -ă/ -ĕ 3л. -ĕ 
 #ед число: -0 ??? -(ă)н ? (во 2 лице) 
 #мн. число: -(ă/ĕ)р (1 и 2 лицо) -ç(ĕ)
-number = ['ăп','ĕп','ăм','ĕм','ăр','ĕр','ам','ем','ар','ер','ăн','ĕн','ĕ','тăр','тĕр','ç','çĕ','чăр','чĕр','сем',]
+number = ['ăп','ĕп','ăм','ĕм','ăр','ĕр','ам','ем','ар','ер','ăн','ĕн','ĕ','тăр','тĕр','ç','чăр','чĕр','сем']
 
 comperetive = ['рах','рех','тарах','терех']
 
-time = ['нӑ','нӗ','кан','кен','ас','ес','м','ма','ман','мас','мес','ме','мен','ççĕ']
+time = ['нӑ','нӗ','кан','кен','ас','ес','ман','мас','мес','ме','мен','ççĕ']
 
 frequency = ['кала','келе']
 
 adverjectper = ['малла','мелле','лă','лĕ','сен','сессӗн','сан','сассӑн']
 
+relative_adj = ['ла','ле']
+
 abst = ['лăх', 'лĕх']
+
+negative = ['м','ма']
+
+verb_pre_future = ['тӑп','тӑн','ть','тпӑр','тӑр','ҫҫӗ','тӗп','тӗн','тпӗр','тӗр']
+
+verb_past_perf = ['рӑм','рӑн','рӗ','рӑмӑр','рӑр','рӗҫ','рӗр']
+
+verb_future = ['ӑп','ӑн','ӗ','ӑпӑр','ӑр','ӗҫ']
 
 for z in chiw:
 
@@ -81,6 +91,45 @@ for z in chiw:
 		lemma = lemma[:-3]
 	
 	if lemma [-4:] in frequency:
+		lemma = lemma[:-4]
+		
+	if lemma [-2:] in relative_adj:
+		lemma = lemma[:-2]
+		
+	if lemma [-2:] in negative:
+		lemma = lemma[:-2]
+		
+	if lemma [-1:] in negative:
+		lemma = lemma[:-1]		
+		
+	if lemma [-1:] in verb_pre_future:
+		lemma = lemma[:-1]
+
+	if lemma [-2:] in verb_pre_future:
+		lemma = lemma[:-2]		
+
+	if lemma [-3:] in verb_pre_future:
+		lemma = lemma[:-3]
+		
+	if lemma [-4:] in verb_pre_future:
+		lemma = lemma[:-4]
+				
+	if lemma [-2:] in verb_past_perf:
+		lemma = lemma[:-2]
+		
+	if lemma [-3:] in verb_past_perf:
+		lemma = lemma[:-3]
+		
+	if lemma [-5:] in verb_past_perf:
+		lemma = lemma[:-5]
+		
+	if lemma [-1:] in verb_future:
+		lemma = lemma[:-1]
+		
+	if lemma [-2:] in verb_future:
+		lemma = lemma[:-2]
+		
+	if lemma [-4:] in verb_future:
 		lemma = lemma[:-4]
 		
 	line[2] = lemma
